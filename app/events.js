f209e1b5b738a15902253cca415e6a078e110420
Oscar Cortes <oscar@nubedeideas.com>
2012-02-25 18:38:33 +0100

    lg1.2

--- app/events.js
+++ app/events.js
@@ -1,5 +1,11 @@
 App.Events = (function(lng, app, undefined) {
 
+	lng.dom('#player').on('tap', function(event) {
+		console.log("Tap Event - Player");
+        LUNGO.Sugar.Sound.background('http://server10.20comunicacion.com:8012/stream.mp3');
+        console.log("On Air - Player");
+    });
+
     return {
 
     }
